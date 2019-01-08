c48a3cb3f2299a6bd542e6d21154a14cce4eb6a7
huangjian <1342042894@qq.com>
2019-01-08 16:47:05 +0800

    add json demo

--- jsonDemo/1/main.js
+++ jsonDemo/1/main.js
@@ -1,1 +1,12 @@
+var fs = require('fs');
+
 console.log("json demo");
+
+fs.readFile('./test.json', 'utf8', function(err, data) {
+	console.log(typeof data, data);
+
+	obj = JSON.parse(data);
+	console.log(typeof obj, obj);
+	console.log(obj.name);
+	console.log(obj.name2);
+});
